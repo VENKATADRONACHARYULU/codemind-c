8ecdbba5c4478afd0f1d3b0570e1ce4317861c53
VENKATADRONACHARYULU <98031481+VENKATADRONACHARYULU@users.noreply.github.com>
2022-06-29 22:03:36 +0530

    # include <stdio.h> int main() { int n,x=0;   scanf("%d",&n);        while (n != 1)        {             if (n % 5 == 0)             {                 n /= 5;             }             else if (n % 3 == 0)             {                 n /= 3;             }             else if (n % 2 == 0)             {                 n /= 2;             }             else             {                 printf("Not Ugly Number");                 x = 1;                 break;             }         } 		        if (x==0) 		        {                 printf("Ugly Number");                 } }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,12 +1,8 @@
 # include <stdio.h>
-# include <string.h>
-
 int main()
 {
 int n,x=0;
-  scanf("%d",&n);
-
-      
+  scanf("%d",&n);  
        while (n != 1) 
        {  
             if (n % 5 == 0) 
